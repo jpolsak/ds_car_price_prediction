80fd9c08584b7f5771a39c66dd2277a335bc17d9
jpolsak <156595088+jpolsak@users.noreply.github.com>
2024-08-04 21:28:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ from sklearn.ensemble import RandomForestRegressor as RF
 import gzip
 import requests
 from io import BytesIO
-import shap
-import matplotlib.pyplot as plt
 
 #Extracción dataset
 df = pd.read_csv('https://raw.githubusercontent.com/jpolsak/datasets/main/car_price_prediction_modif.csv')
